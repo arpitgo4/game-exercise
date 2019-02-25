6a8586a4446ca5d33b6da64ef6d6c00988833fa4
Arpit Goyal <arpit.go4@gmail.com>
2019-02-25 14:11:14 +0530

    check added for already played games

--- server/controllers/game.ts
+++ server/controllers/game.ts
@@ -27,6 +27,21 @@ import { CustomError } from 'Interfaces';
 import * as constants from '../utils/constants';
 
 
+
+export const getGame = (user_id: string, game_id: string): Promise<IGameModel> => {
+    return User.findOne({ user_id, 'games.game_id': game_id }, { games: { $elemMatch: { game_id } } })
+    // @ts-ignore
+    .then((user: IUserModel) => {
+        if (!user)
+            return Promise.reject({ message: `invalid user` });
+
+        if (user.games.length === 0)
+            return Promise.reject({ message: `invalid game` });
+
+        return user.games[0];
+    });
+};
+
 export const startGame = (user_id: string) => {
     return userCtrl.getUser(user_id, { games: false, })
     .then((user: IUserModel) => {
@@ -39,7 +54,7 @@ export const startGame = (user_id: string) => {
 
         const new_game = {
             game_id: uuid.v4(),
-            score: 0,
+            score: -1,
         };
 
         if (last_game_date === current_date) {
@@ -73,16 +88,26 @@ export const startGame = (user_id: string) => {
     });
 };
 
-// conditionally update highest score
+
 export const endGame = (user_id: string, game_id: string, score: number) => {
-    return User.findOneAndUpdate(
-        { user_id, 'games.game_id': game_id, },
-        { $set: { 'games.$.score': score } },
-        {
-            new: true,
-            select: {
-                games: { $elemMatch: { game_id } }
-        }
+    return getGame(user_id, game_id)
+    .then((game: IGameModel) => {
+        if (game.score !== -1)
+            return Promise.reject({ message: `game already played!` });
+
+        return Promise.resolve();
+    })
+    .then(() => {
+        return User.findOneAndUpdate(
+            { user_id, 'games.game_id': game_id, },
+            { $set: { 'games.$.score': score, }, $max: { 'meta.highest_score': score, } },
+            {
+                new: true,
+                select: {
+                    games: { $elemMatch: { game_id } }
+                }
+            }
+        );
     })
     // @ts-ignore
     .then((user: IUserModel) => {
